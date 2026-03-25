90823e2883cb8df5092e707ece108b1e7fff356e
mohdazam0786 <mohdazam0123sid@gmail.com>
2026-03-25 17:04:48 +0530

    working on Task Controller and its routes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,9 +1,11 @@
 import { User } from "../models/user.models.js"
+import { ProjectMember } from "../models/projectmember.models.js"
 import {ApiError} from "../utils/api-error.js"
 import {asyncHandler} from "../utils/async-handler.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
-export const verifyJWT = asyncHandler(async(req, resizeBy, next) => {
+export const verifyJWT = asyncHandler(async(req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
     if(!token){
@@ -36,3 +38,35 @@ export const verifyJWT = asyncHandler(async(req, resizeBy, next) => {
         )
     }
 })
+
+
+export const validateProjectPermission = (roles = []) => {
+    return asyncHandler(async (req, res, next) => {
+        const {projectId} = req.params;
+
+        if(!projectId){
+            throw new ApiError(400, "Project Id is missing");
+        }
+
+        const project = await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id)
+        })
+
+        if(!project){
+            throw new ApiError(400, "Project not found");
+        }
+
+        const givenRole = project?.role
+        req.user.role = givenRole
+
+        if(!roles.includes(givenRole)){
+            throw new ApiError(
+                403, 
+                "You do not have permission to perform this action"
+            )
+        }
+
+        next()
+    })
+}
--- src/validators/index.js
+++ src/validators/index.js
@@ -1,5 +1,5 @@
 import { body } from "express-validator";
-
+import { AvailableUserRole } from "../utils/constants.js"
 
 
 const userRegisterValidator = () => {
@@ -70,13 +70,35 @@ const userResetForgotPasswordValidator = () => {
     ]
 }
 
+const createProjectValidator = () => {
+    return [
+        body("name").notEmpty().withMessage("Name is Required"),
+        body("description").optional()
+    ]
+}
 
+const addMembertoProjectValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("role")
+            .notEmpty()
+            .withMessage("Role is required")
+            .isIn(AvailableUserRole)
+            .withMessage("Role is invalid")
 
-
+    ]
+}
 export {
     userRegisterValidator,
     userLoginValidator,
     userChangeCurrentPasswordValidator,
     userForgotPasswordValidator,
-    userResetForgotPasswordValidator
+    userResetForgotPasswordValidator,
+    createProjectValidator,
+    addMembertoProjectValidator
 }
